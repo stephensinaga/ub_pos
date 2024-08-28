669f0614e1cb4a299a6d3352e8280ef242c34e75
KeyshaRachelia <155528149+KeyshaRachelia@users.noreply.github.com>
2024-08-28 09:47:51 +0700

    UB menambahkan template dan login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\SessionController;
+use App\Http\Controllers\AuthController;
+// use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +16,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('', [AdminController::class, 'CreateProductView'])->name('CreateProductView');
-Route::post('admin/create/product', [AdminController::class, 'CreateProduct'])->name('CreateProductProcess');
-Route::delete('admin/delete/product/{id}', [AdminController::class, 'DeleteProduct'])->name('DeleteProduct');
-Route::get('admin/edit/product/view/{id}', [AdminController::class, 'EditProductView'])->name('EditProductView');
-Route::put('admin/edit/product/{id}', [AdminController::class, 'EditProduct'])->name('EditProductProcess');
+// Route::get('', [AdminController::class, 'CreateProductView'])->name('CreateProductView');
+// Route::post('admin/create/product', [AdminController::class, 'CreateProduct'])->name('CreateProductProcess');
+// Route::delete('admin/delete/product/{id}', [AdminController::class, 'DeleteProduct'])->name('DeleteProduct');
+// Route::get('admin/edit/product/view/{id}', [AdminController::class, 'EditProductView'])->name('EditProductView');
+// Route::put('admin/edit/product/{id}', [AdminController::class, 'EditProduct'])->name('EditProductProcess');
+
+
+Route::controller(AuthController::class)->group(function () {
+	Route::get('register', 'register')->name('register');
+	Route::post('register', 'registerSimpan')->name('register.simpan');
+	Route::get('', 'login')->name('login');
+	Route::post('login', 'loginAksi')->name('login.aksi');
+	Route::get('logout', 'logout')->middleware('auth')->name('logout');
+});
+
+Route::middleware('auth')->group(function () {
+	Route::get('dashboard', function () {
+		return view('dashboard');
+	})->name('dashboard');
+});
